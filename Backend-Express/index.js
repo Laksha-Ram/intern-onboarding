de254f6d5b13b758c138e2cbbd9fd0f1b3710e3c
Lakshasree Ramkumar <lakshasreeramkumar@gmail.com>
2026-01-18 22:01:58 +0530

    Day 9: prep backend for deployment

--- Backend-Express/index.js
+++ Backend-Express/index.js
@@ -1,12 +1,12 @@
 const express = require("express");
 const app = express();
 
-const PORT = 5000;
+const PORT = process.env.PORT || 5000;
 
 app.get("/health", (req, res) => {
   res.send("Server is healthy ;)");
 });
 
 app.listen(PORT, () => {
-  console.log(`Server running on http://localhost:${PORT}`);
+  console.log(`Server running on port ${PORT}`);
 });
